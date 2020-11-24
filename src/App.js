e2e27ddafbe0fb816e90863837dbf4aa2a4318b9
FaheemRiaz1 <faheemriaz177@gmail.com>
2020-11-24 14:09:19 +0500

    mesage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
 import React from 'react';
+
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+function Header1(props) {
+  return <h1>  {props.name}</h1>;
+}
+function Information(props) {
+  return <h1>Age:  {props.age} <br></br>
+  Address: {props.address}<br></br>
+  Phone Number: {props.phoneNumber}</h1>;
+}
+function Education(props){
+return <h1> {props.Institute} <br></br>
+ Degree: {props.degree} <br></br>
+ Start Date: {props.StartDate} <br></br>
+ End Date: {props.EndDate} <br></br>
+ Description: {props.description}</h1>
+}
+function Experience(props){
+return <h1> {props.company}<br></br>
+Start Date: {props.StartDate1} <br></br>
+ End Date: {props.EndDate1} <br></br>
+</h1>
+
+}
+function Skills(props){
+  return <h1> {props.hobbies}</h1>
+}
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <div class="header">
+
+       <table>
+       <tr id="">
+            <td id="tab">
+             <Header1  name="Faheem Riaz" />
+
+            </td>
+            <td >  <img  class ="photo" src="https://bmc.edu.pk/wp-content/uploads/2019/03/doctor-red-1.png" />
+            </td>
+          </tr>
+  
+
+
+       </table>
+
+       
+      </div>
+
+      
+      <div class="Information">
+     
+      <h1 class="heading">Information:</h1>
+      <Information  age="20" address="house" phoneNumber="034567790"/>
+      
+      </div>
+      <div class="Education">
+
+      <h1 class="heading">Education:</h1>
+      <Education Institue="IMCB"degree="FSC" StartDate="10-10-2020"
+      EndDate="10-10-2021" description="IMCB"/>
+     
+     <Education Institue="COMSATS"degree="BSCS" StartDate="10-10-2022"
+      EndDate="10-10-2026" description="COMSATS UNIVERSITY"/>
+
+      </div>
+      
+    <div class="Company">
+      <h1 class="heading">Experience:</h1>
+      <Experience company="TECH" StartDate1="10-10-2022"
+      EndDate1="10-10-2026" />
+    </div>
+    <div class="Hobbies">
+    <h1 class="heading">Hobbies:</h1>
+    <Skills hobbies="cricket"/>
+    <Skills hobbies="gaming"/>
+    <Skills hobbies="writing"/>
+    </div>
+    
     </div>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+// const element=<header1 name="Faheem"/>;
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
+
+// ReactDOM.render(<header1 name="Faheem"/>,document)
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
